95919ba947211fbba964b9ef9cc3a70915276b8b
Maria Fernanda Catani <nekofuroshiki@gmail.com>
2023-05-29 20:29:41 -0300

    Calcular Média

--- src/CalcularMedia.java
+++ src/CalcularMedia.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class CalcularMedia {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalcularMedia extends JFrame{
+    public JLabel PrimeiraNota;
+    private JTextField textPrimeiraNota;
+    public JLabel SegundaNota;
+    private JTextField textSegundaNota;
+    private JButton buttonCalcularMedia;
+    private JTextField textMedia;
+    public JLabel Media;
+    private JTextField textSituacao;
+    public JLabel Situacao;
+    public JPanel formCalcularMedia;
+
+    public CalcularMedia() {
+    buttonCalcularMedia.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Double valorMedia = 0d;
+            valorMedia = (Double.valueOf(textPrimeiraNota.getText()) + Double.valueOf(textSegundaNota.getText())) / 2;
+            textMedia.setText(valorMedia.toString());
+
+            if (valorMedia < 7) {
+                textSituacao.setText("Reprovado");
+            }
+                else if (valorMedia >= 7){
+                    textSituacao.setText("Aprovado");
+            }
+        }
+    });
+}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalcularMedia form = new CalcularMedia();
+        form.setContentPane(form.formCalcularMedia);
+        form.setVisible(true);
+        form.setSize(500,230);
+        form.setTitle("Calcular Média");
     }
 }
